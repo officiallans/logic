06f9d956fdcae967342b279d30cf58899955961a
Vova798521 <0192837465z8@gmail.com>
2020-03-24 19:52:47 +0200

    Edit code

--- index.js
+++ index.js
@@ -1,84 +1,48 @@
-// var express = require('express'); var path = require('path'); // модуль для парсинга шляху
-// var app = express();
-
-// //app.use(express.favicon()); // отдаем стандартную фавиконку, можем здесь же свою задать 
-// //app.use(express.logger('dev')); // выводим все запросы со статусами в консоль 
-// //app.use(express.bodyParser()); // стандартный модуль, для парсинга JSON в запросах 
-// //app.use(express.methodOverride()); // поддержка put и delete 
-// //app.use(app.router); // модуль для простого задания обработчиков путей 
-// app.use(express.static(path.join(__dirname, "public"))); // запуск статического файлового сервера, который смотрит на папку public/ (в нашем случае отдает index.html)
-// app.get('/api', function (req, res) {
-// res.send('API is running');
-// });
-// app.listen(1338, function(){ 
-// console.log('Express server listening on port 1338');
-//    });
-// var express = require('express');
-// var app = express(); 
-// app.listen(1338, function(){
-// console.log('Express server listening on port 1338');
-// });
-
 var favicon = require('serve-favicon');
 var express = require('express');
-var path = require('path'); // модуль для парсинга пути
+var path = require('path'); 
 var log = require('./libs/log')(module);
 var app = express();
-app.use(express.favicon("public/favicon.ico")); // отдаем стандартную фавиконку, можем здесь же свою задать 
-
-app.use(express.logger('dev')); // выводим все запросы со статусами в консоль
- app.use(express.bodyParser()); // стандартный модуль, для парсинга JSON в запросах 
+app.use(express.favicon("public/favicon.ico")); 
+app.use(express.logger('dev')); 
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.methodOverride()); // поддержка put и delete 
 app.use(app.router); // модуль для простого задания обработчиков путей 
 app.use(express.static(path.join(__dirname, "public"))); // запуск статического файлового сервера, который смотрит на папку public/ (в нашем случае отдает index.html)
-
 app.get('/api', function (req, res) { 
-res.send('API is running');
+	res.send('API is running');
 });
-app.listen(1338, function(){
-log.info('Express server listening on port 1338');
+app.listen(1337, function(){
+	log.info('Express server listening on port 1338');
 });
 //=============================================================================================
 app.use(function(req, res, next){
-res.status(404);
-log.debug('Not found URL: %s',req.url);
-res.send({ error: 'Not found' });
-return;
+	res.status(404);
+	log.debug('Not found URL: %s',req.url);
+	res.send({ error: 'Not found' });
+	return;
 });
  
 app.use(function(err, req, res, next){ 
-res.status(err.status || 500);
-log.error('Internal error(%d): %s',res.statusCode,err.message); res.send({ error: err.message });
+	res.status(err.status || 500);
+	log.error('Internal error(%d): %s',res.statusCode,err.message); res.send({ error: err.message });
  	return;
 });
 
 app.get('/ErrorExample', function(req, res, next){
 next(new Error('Random error!'));
 });
-// //=============================================================================================
-// app.get('/api/articles', function(req, res) {
-// res.send('This is not implemented get now');
-// });
-// app.post('/api/articles', function(req, res) {
-//  	res.send('This is not implemented post now');
-// });
-// app.get('/api/articles/:id', function(req, res) {
-// res.send('This is not implemented get id now');
-// });
-// app.put('/api/articles/:id', function (req, res){
-// res.send('This is not implemented put now');
-// });
-// app.delete('/api/articles/:id', function (req, res){
-// res.send('This is not implemented delete now');
-// });
 
 var config          = require('./libs/config');
 app.listen(config.get('port'), function(){
     log.info('Express server listening on port ' + config.get('port'));
 });
 
-var log             = require('./libs/log')(module);
-var ArticleModel    = require('./libs/mongoose').ArticleModel;
+var log = require('./libs/log')(module);
+var ArticleModel = require('./libs/mongoose').ArticleModel;
 
 app.get('/api/articles', function(req, res) {
     return ArticleModel.find(function (err, articles) {
@@ -92,12 +56,12 @@ app.get('/api/articles', function(req, res) {
     });
 });
 
-app.post('/api/articles', function(req, res) {
+app.post('/api/articles', jsonParser, function (req, res) {
     var article = new ArticleModel({
         title: req.body.title,
         author: req.body.author,
         description: req.body.description,
-        images: req.body.images
+        images: []
     });
 
     article.save(function (err) {
@@ -141,10 +105,10 @@ app.put('/api/articles/:id', function (req, res){
             return res.send({ error: 'Not found' });
         }
 
-        article.title = req.body.title;
-        article.description = req.body.description;
-        article.author = req.body.author;
-        article.images = req.body.images;
+		article.title = req.body.title;
+		article.description = req.body.description;
+		article.author = req.body.author;
+		article.images = req.body.images;
         return article.save(function (err) {
             if (!err) {
                 log.info("article updated");
